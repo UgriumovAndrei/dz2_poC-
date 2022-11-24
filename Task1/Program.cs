7d1c3c68000afa3c7b7ae44ef09f18547076f06b
ANDREI <ugryumovandrej@yandex.ru>
2022-11-25 00:36:42 +0300

    added sth into T1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,5 +2,5 @@
 // и на выходе показывает вторую цифру этого числа.
 
 int N = Convert.ToInt32(Console.ReadLine());
-int LD = (N / 10) % 10;
-Console.Write(LD);
+int LastD = (N / 10) % 10;
+Console.Write(LastD);
