2d1cd72c76572d93b5256fac536563d12901687b
ANDREI <ugryumovandrej@yandex.ru>
2022-11-24 14:55:16 +0300

    Task1 done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзн число
+// и на выходе показывает вторую цифру этого числа.
+
+int N = Convert.ToInt32(Console.ReadLine());
+int LD = (N / 10) % 10;
+Console.Write(LD);
